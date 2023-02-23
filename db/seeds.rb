d6199ee1782b6156d3e3d06383f5e91cbf9ad99a
delphine abouab <delphine.abouab@outlook.fr>
2023-02-23 12:56:49 +0100

    Seed ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'open-uri'
+require 'json'
+
+Movie.destroy_all
+
+url = 'https://tmdb.lewagon.com/movie/top_rated'
+img_url = 'https://image.tmdb.org/t/p/w500'
+movies = JSON.parse(URI.open(url).read)['results']
+
+movies.each do |movie|
+  title = movie['title']
+  overview = movie['overview']
+  poster_url = img_url + movie['poster_path']
+  rating = movie['vote_average']
+  Movie.create({ title:, overview:, poster_url:, rating: })
+end
+
+url = 'https://tmdb.lewagon.com/movie/popular'
+img_url = 'https://image.tmdb.org/t/p/w500'
+movies = JSON.parse(URI.open(url).read)['results']
+
+movies.each do |movie|
+  title = movie['title']
+  overview = movie['overview']
+  poster_url = img_url + movie['poster_path']
+  rating = movie['vote_average']
+  Movie.create({ title:, overview:, poster_url:, rating: })
+end
